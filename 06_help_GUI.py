4138017c876fec4f0edd48df94b95d6955cf41d4
bensalmontree <78379397+bensalmontree@users.noreply.github.com>
2023-03-01 09:58:50 +1300

    base

--- 06_help_GUI.py
+++ 06_help_GUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from functools import partial # To prevent unwanted windows
 
 class Converter:
 
@@ -18,16 +19,20 @@ class Converter:
         self.to_help_button = Button(self.button_frame, text="Help / Info", bg="#CC6600", fg=button_fg, font=button_font, width=12, command=self.to_help)
         self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
 
-    @staticmethod
-    def to_help():
-        DisplayHelp()
+    def to_help(self):
+        DisplayHelp(self)
 
 class DisplayHelp:
 
-    def __init__(self):
+    def __init__(self, partner):
         background = "#ffe6cc"
-
         self.help_box = Toplevel()
+
+        # disable help button
+        partner.to_help_button.config(state=DISABLED)
+
+        # IF users press cross at top, closes help and 'released' help button
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help,partner))
 
         self.help_frame = Frame(self.help_box, width=300, height=200, bg=background)
         self.help_frame.grid()
@@ -39,8 +44,14 @@ class DisplayHelp:
         self.help_text_label = Label(self.help_frame, bg=background, text=help_text, wrap=350, justify="left")
         self.help_text_label.grid(row=1, padx=10)
         
-        self.dismiss_button = Button(self.help_frame, font=("Arial", "12", "bold"), text="Dismiss", bg="#CC6600", fg="#FFFFFF")
+        self.dismiss_button = Button(self.help_frame, font=("Arial", "12", "bold"), text="Dismiss", bg="#CC6600", fg="#FFFFFF", command=partial(self.close_help,partner))
         self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+    # closes help dialogue (used by button and x at top of dialogue)
+    def close_help(self, partner):
+        # Put help button back to normal
+        partner.to_help_button.config(state=NORMAL)
+        self.help_box.destroy()
 
 
 # main routine
