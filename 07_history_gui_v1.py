232edf51a0dc6a7accf3a645460fc117695f9f9f
bensalmontree <78379397+bensalmontree@users.noreply.github.com>
2023-03-03 13:26:15 +1300

    Add files via upload

--- 07_history_gui_v1.py
+++ 07_history_gui_v1.py
@@ -4,51 +4,42 @@ from functools import partial # To prevent unwanted windows
 class Converter:
 
     def __init__(self):
-
-        # Initialise variables (such as the feedback variable)
-        self.var_feedback = StringVar()
-        self.var_feedback.set("")
-
-        self.var_has_error = StringVar()
-        self.var_has_error.set("no")
-
-        self.all_calculations = []
-
         # common format for all buttons
         # Arial size 14 bold, with white text
         button_font = ("Arial", "12", "bold")
         button_fg = "#FFFFFF"
 
+        # Five item list
+        self.all_calculations = ['0 F° is -18 C°']
+
         # Set up GUI Frame
         self.temp_frame = Frame()
         self.temp_frame.grid()
 
-        self.temp_heading = Label(self.temp_frame, text="Temperature Converter", font=("Arial", "16", "bold"))
-        self.temp_heading.grid(row=0)
+        self.button_frame = Frame(padx=30, pady=30)
+        self.button_frame.grid(row=0)
 
-        self.temp_entry = Entry(self.temp_frame, font=("Arial","14"))
-        self.temp_entry.grid(row=2, padx=10, pady=10)
-
-        error = "Please enter a number"
-        self.output_label = Label(self.temp_frame, text="", fg="#9C0000")
-        self.output_label.grid(row=3)
-
-        # Converseion, help and history / export buttons
-        self.button_frame = Frame(self.temp_frame)
-        self.button_frame.grid(row=4)
-
-        self.to_history_button = Button(self.button_frame, text="History / Export", bg="#004C99", fg=button_fg, font=button_font, width=12, state=DISABLED, command=self.to_history)
+        self.to_history_button = Button(self.button_frame, text="History / Export", bg="#004C99", fg=button_fg, font=button_font, width=12, state=DISABLED, command=lambda: self.to_history(self.all_calculations))
         self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
 
         # *** Remove when integrating!! ***
         self.to_history_button.config(state=NORMAL)
 
-    def to_history(self):
-        HistoryExport(self)
+    def to_history(self, all_calculations):
+        HistoryExport(self, all_calculations)
     
 class HistoryExport:
 
-    def __init__(self, partner):
+    def __init__(self, partner, calc_list):
+
+        # set maximum number of calculations to 5 this can be changed if we want to show fewer / more calculations
+        max_calcs=5
+        self.var_max_calcs = IntVar()
+        self.var_max_calcs.set(max_calcs)
+
+        # FUnction converts contents of calculations list into a string.
+        calc_string_text = self.get_calc_string(calc_list)
+
         # setup diaglogue box and background colour
         self.history_box = Toplevel()
 
@@ -63,6 +54,36 @@ class HistoryExport:
 
         self.dismiss_button = Button(self.history_frame, font=("Arial", "12", "bold"), text="Dismiss", bg="#666666", fg="#FFFFFF", command=partial(self.close_history, partner))
         self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+        # History text and label
+        hist_text = "Below are your recent calculations - showing 3/3 calculations. All calculations are shown to the nearest degree"
+        self.text_instructions_label = Label(self.history_frame, text=hist_text, width=45, justify="left", wraplength=300, padx=10, pady=10)
+        self.text_instructions_label.grid(row=1)
+
+        self.all_calcs_label = Label(self.history_frame, text="calculations go here", padx=10, pady=10, bg="ffe6cc", width=40, justify="left")
+        self.all_calcs_label.grid(row=2)
+
+        # instructions for saving files
+        save_text = "Either choose a custom file name (and push <Export>) or simply push <Export> to save your calculations in a text file. If the filename already exists, it will be overwritten!"
+        self.save_instructions_label = Label(self.history_frame, text=save_text, wraplength=300, justify="left", width=40, padx=10, pady=10)
+        self.save_instructions_label.grid(row=3)
+
+        # Filename entry widget, white background to start
+        self.filename_entry = Entry(self.history_frame, font=("Arial", "14"), bg="#ffffff", width=25)
+        self.filename_entry.grid(row=4, padx=10, pady=10)
+
+        self.filename_error_label = Label(self.history_frame, text="Filename error goes here", fg="#9C0000", font=("Arial", "12", "bold"))
+        self.filename_error_label.grid(row=5)
+
+        self.button_frame=Frame(self.history_frame)
+        self.button_frame.grid(row=6)
+
+        self.export_button = Button(self.button_frame, font=("Arial", "12", "bold"), text="Export", bg="#004C99", fg="#FFFFFF", width=12)
+        self.export_button.grid(row=0, column=0, padx=10, pady=10)
+
+        self.dismiss_button = Button(self.button_frame, font=("Arial", "12", "bold"), text="Dismiss", bg="#666666", fg="#FFFFFF", width=12, command=partial(self.close_history, partner))
+        self.dismiss_button.grid(row=0, column=1, padx=10, pady=10)
+
     
     # closes help dialogue ( used by button and x at top of dialogue)
     def close_history(self, partner):
